af3af143958de14c6cae44c42c01bce5d72c86c4
Arslanoov <rasularslanoov@gmail.com>
2021-04-29 18:21:00 +0500

    build: Added jest

---
+++ webpack.config.demo.js
@@ -0,0 +1,51 @@
+const path = require("path")
+
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "demo", "index.ts"),
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "demo/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main-[hash:8].css",
+    })
+  ],
+
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.(css)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(s[ac]ss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ],
+      }
+    ]
+  },
+
+  devServer: {
+    open: true,
+    host: "0.0.0.0",
+    port: 8088,
+    hot: true,
+    inline: true,
+  }
+}
